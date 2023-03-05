cc1fc743c7326b47f9093e3180fd67e0ecf48bda
vareee <tikhrv@gmail.com>
2023-03-05 16:10:07 +0500

    second ver

--- chess.cpp
+++ chess.cpp
@@ -5,42 +5,40 @@
 using namespace std;
 
 int main() {
-    char king_pos[3];
-    char bishop_pos[3];
-    char rook_pos[3];
-    string alf = "abcdefgh";
+    char king_pos[3]; //coordinates of the king
+    char bishop_pos[3]; //coordinates of the bishop
+    char rook_pos[3]; //coordinates of the rook
+    string alf = "abcdefgh"; // posibble values of the desk columns
     cout << "Enter the positions of the chess pieces in the following format: 'B2' (the first symbol is the column name (letter from A to H), the second is the row number (number from 1 to 8))\n";
     cout << "Enter the position of the king: ";
     cin.getline(king_pos, 3);
     king_pos[0] = tolower(king_pos[0]);
-    if (alf.find(king_pos[0]) == string::npos || int(king_pos[1]) - 48 > 8 || int(king_pos[1]) - 48 < 1)
+    if (alf.find(king_pos[0]) == string::npos || int(king_pos[1]) - 48 > 8 || int(king_pos[1]) - 48 < 1) //if the postion does not exist
     {
-        cout << "error\n";
+        cout << "Error! Incorrect format of the entered data!\n";
         exit(1);
     }
     cout << "Enter the position of the bishop: ";
     cin.getline(bishop_pos, 3);
     bishop_pos[0] = tolower(bishop_pos[0]);
-    if (alf.find(bishop_pos[0]) == string::npos || int(bishop_pos[1]) - 48 > 8 || int(bishop_pos[1]) - 48 < 1)
+    if (alf.find(bishop_pos[0]) == string::npos || int(bishop_pos[1]) - 48 > 8 || int(bishop_pos[1]) - 48 < 1) //if the postion does not exist
     {
-        cout << "error\n";
+        cout << "Error! Incorrect format of the entered data!\n";
         exit(1);
     }
     cout << "Enter the position of the rook: ";
     cin.getline(rook_pos, 3);
     rook_pos[0] = tolower(rook_pos[0]);
-    if (alf.find(rook_pos[0]) == string::npos || int(rook_pos[1]) - 48 > 8 || int(rook_pos[1]) - 48 < 1)
+    if (alf.find(rook_pos[0]) == string::npos || int(rook_pos[1]) - 48 > 8 || int(rook_pos[1]) - 48 < 1) //if the postion does not exist
     {
-        cout << "error\n";
+        cout << "Error! Incorrect format of the entered data!\n";
         exit(1);
     }
-    // cout << king_pos << endl;
-    // cout << king_pos[0] << rook_pos[0] << king_pos[1] << rook_pos[1] << endl;
     bool rook = rook_threat(king_pos, rook_pos);
     bool bishop = bishop_threat(king_pos, bishop_pos);
     if (rook) {
         if (bishop)
-            cout << "Error!";
+            cout << "Error! Check from two pieces at the same time!";
         else
             cout << "Check from the rook!";
     }
@@ -49,6 +47,5 @@ int main() {
         cout << "Check from the bishop!";
     else
         cout << "No check!";
-    // cout << int('b');
 
 }
--- chess_func.cpp
+++ chess_func.cpp
@@ -3,14 +3,14 @@
 
 using namespace std;
 
-bool bishop_threat(char king_pos[3], char bishop_pos[3]) {
+bool bishop_threat(char king_pos[3], char bishop_pos[3]) { //returns true if the check comes from the bishop
     bool check = false;
     if (abs(int(king_pos[0]) - int(bishop_pos[0])) == abs(int(king_pos[1]) - int(bishop_pos[1])) )
         check = true;
     return check;
 }
 
-bool rook_threat(char king_pos[3], char rook_pos[3]) {
+bool rook_threat(char king_pos[3], char rook_pos[3]) { //returns true if the check comes from the rook
     bool check = false;
     if (king_pos[0] == rook_pos[0] || king_pos[1] == rook_pos[1])
         check = true;
